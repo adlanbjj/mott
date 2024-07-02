b1a36b124099dddc20fd8c1be082ce7313924220
adlan isaev <adlanisaev@MacBook-Air-de-adlan.local>
2024-07-02 17:42:39 +0200

    Updated API URLs and cleaned up server configurations

--- client/src/components/Posts/PostList.jsx
+++ client/src/components/Posts/PostList.jsx
@@ -264,7 +264,7 @@ const PostList = () => {
                     onClick={() => handleCommentToggle(post._id)}
                     className="btn btn-comment"
                   >
-                    <FontAwesomeIcon icon={faComment} className="icon" /> Comments{" "}
+                    <FontAwesomeIcon icon={faComment} className="icon" /> Kommentareş{" "}
                     {post.comments.length}
                   </button>
                   <div className="btn-container">
